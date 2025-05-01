6a8910e345aad46b7a8b0961a0a9a3edc7e7aed2
fResult <styxmaz@gmail.com>
2025-05-02 02:26:39 +0700

    Create \`DataSourceCustomerService\` class and use it instead of Hard-code `DevelopmentOnlyCustomerService`.

--- kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
+++ kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
@@ -1,14 +1,18 @@
 package com.fResult.bootstrap
 
-import com.fResult.bootstrap.customers.services.DevelopmentOnlyCustomerService
+import com.fResult.bootstrap.common.DataSourceUtils
+import com.fResult.bootstrap.customers.services.DataSourceCustomerService
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 
 @SpringBootApplication
 class BootstrapApplication
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-	val customerService = DevelopmentOnlyCustomerService()
-
-	Demo.workWithCustomerService(BootstrapApplication::class, customerService)
+  EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build()
+    .let(DataSourceUtils::initializeDdl)
+    .let(::DataSourceCustomerService)
+    .also { Demo.workWithCustomerService(BootstrapApplication::class, it) }
 }
