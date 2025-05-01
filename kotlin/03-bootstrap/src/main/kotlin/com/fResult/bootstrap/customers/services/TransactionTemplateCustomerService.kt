a785db6bd743bd685b1ce87f2ccd3f694b918646
fResult <styxmaz@gmail.com>
2025-05-02 02:48:10 +0700

    Create `TransactionTemplateCustomerService` for Kotlin 03 Bootstrap.

---
+++ kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/customers/services/TransactionTemplateCustomerService.kt
@@ -0,0 +1,18 @@
+package com.fResult.bootstrap.customers.services
+
+import com.fResult.bootstrap.customers.Customer
+import org.springframework.transaction.support.TransactionTemplate
+import javax.sql.DataSource
+
+class TransactionTemplateCustomerService(
+  dataSource: DataSource,
+  private val transactionTemplate: TransactionTemplate,
+) : BaseCustomerService(dataSource) {
+
+  override fun save(vararg names: String): Collection<Customer> =
+    transactionTemplate.execute { super.save(*names) } ?: listOf()
+
+  override fun findById(id: Long): Customer? = transactionTemplate.execute { super.findById(id) }
+
+  override fun findAll(): Collection<Customer> = transactionTemplate.execute { super.findAll() } ?: listOf()
+}
