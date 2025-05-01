176a1f94ac4ffba4900e59e3e17f41193e67f3e4
fResult <styxmaz@gmail.com>
2025-05-02 02:57:12 +0700

    Utilize `TransactionTemplateCustomerService` for Kotlin 03 Bootstrap.

--- kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
+++ kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
@@ -1,18 +1,26 @@
 package com.fResult.bootstrap
 
 import com.fResult.bootstrap.common.DataSourceUtils
-import com.fResult.bootstrap.customers.services.DataSourceCustomerService
+import com.fResult.bootstrap.customers.services.TransactionTemplateCustomerService
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.transaction.support.TransactionTemplate
 
 @SpringBootApplication
 class BootstrapApplication
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-  EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build()
-    .let(DataSourceUtils::initializeDdl)
-    .let(::DataSourceCustomerService)
-    .also { Demo.workWithCustomerService(BootstrapApplication::class, it) }
+  EmbeddedDatabaseBuilder()
+    .setType(EmbeddedDatabaseType.H2)
+    .build()
+    .let {
+      val initializedDataSource = DataSourceUtils.initializeDdl(it)
+      val dsTxManager = DataSourceTransactionManager(initializedDataSource)
+      val transactionTemplate = TransactionTemplate(dsTxManager)
+
+      TransactionTemplateCustomerService(initializedDataSource, transactionTemplate)
+    }.also { Demo.workWithCustomerService(BootstrapApplication::class, it) }
 }
