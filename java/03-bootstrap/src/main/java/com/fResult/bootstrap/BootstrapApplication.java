e63ca28fbe34a0eeae00728defac5691ce48e2d9
fResult <styxmaz@gmail.com>
2025-05-03 00:08:39 +0700

    Utilize `TransactionTemplateCustomerService` class for Java.

--- java/03-bootstrap/src/main/java/com/fResult/bootstrap/BootstrapApplication.java
+++ java/03-bootstrap/src/main/java/com/fResult/bootstrap/BootstrapApplication.java
@@ -1,19 +1,35 @@
 package com.fResult.bootstrap;
 
 import com.fResult.bootstrap.common.DataSourceUtils;
-import com.fResult.bootstrap.customers.services.DataSourceCustomerService;
+import com.fResult.bootstrap.customers.services.CustomerService;
+import com.fResult.bootstrap.customers.services.TransactionTemplateCustomerService;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootApplication
 public class BootstrapApplication {
   public static void main(String[] args) {
     final var dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+
+    Optional.of(dataSource)
+        .map(BootstrapApplication::buildCustomerService)
+        .ifPresent(BootstrapApplication::workWithCustomerServiceConsumer);
+  }
+
+  private static CustomerService buildCustomerService(DataSource dataSource) {
     final var initializedDataSource = DataSourceUtils.initializeDdl(dataSource);
+    final var dsTxManager = new DataSourceTransactionManager(initializedDataSource);
+    final var transactionTemplate = new TransactionTemplate(dsTxManager);
 
-    final var customerService = new DataSourceCustomerService(initializedDataSource);
+    return new TransactionTemplateCustomerService(initializedDataSource, transactionTemplate);
+  }
 
+  private static void workWithCustomerServiceConsumer(CustomerService customerService) {
     Demo.workWithCustomerService(BootstrapApplication.class, customerService);
   }
 }
