cc5725457f37c9dfef91dac19327e7404fbf0b27
fResult <styxmaz@gmail.com>
2025-05-04 02:16:57 +0700

    Implement `BootstrapApplication` main method for the application context.

--- java/03-bootstrap/src/main/java/com/fResult/bootstrap/BootstrapApplication.java
+++ java/03-bootstrap/src/main/java/com/fResult/bootstrap/BootstrapApplication.java
@@ -1,33 +1,41 @@
 package com.fResult.bootstrap;
 
-import com.fResult.bootstrap.common.DataSourceUtils;
+import com.fResult.bootstrap.common.SpringUtils;
+import com.fResult.bootstrap.common.configs.DataSourceConfiguration;
 import com.fResult.bootstrap.customers.services.CustomerService;
 import com.fResult.bootstrap.customers.services.TransactionTemplateCustomerService;
-import java.util.Optional;
 import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
+@Configuration
+@Import(DataSourceConfiguration.class)
 public class BootstrapApplication {
   public static void main(String[] args) {
-    final var dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    final var context = SpringUtils.run(BootstrapApplication.class, "prod");
 
-    Optional.of(dataSource)
-        .map(BootstrapApplication::buildCustomerService)
-        .ifPresent(BootstrapApplication::workWithCustomerServiceConsumer);
-  }
-
-  private static CustomerService buildCustomerService(DataSource dataSource) {
-    final var initializedDataSource = DataSourceUtils.initializeDdl(dataSource);
-    final var dsTxManager = new DataSourceTransactionManager(initializedDataSource);
-    final var transactionTemplate = new TransactionTemplate(dsTxManager);
-
-    return new TransactionTemplateCustomerService(initializedDataSource, transactionTemplate);
-  }
-
-  private static void workWithCustomerServiceConsumer(CustomerService customerService) {
+    final var customerService = context.getBean(CustomerService.class);
     Demo.workWithCustomerService(BootstrapApplication.class, customerService);
+  }
+
+  @Bean
+  PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+  @Bean
+  TransactionTemplateCustomerService customerService(
+      DataSource dataSource, TransactionTemplate transactionTemplate) {
+
+    return new TransactionTemplateCustomerService(dataSource, transactionTemplate);
+  }
+
+  @Bean
+  TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+    return new TransactionTemplate(transactionManager);
   }
 }
