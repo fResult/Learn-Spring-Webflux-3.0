2099cd8d9e6fd29953852134662a4abfb0d94f52
fResult <styxmaz@gmail.com>
2025-05-04 02:13:55 +0700

    Fix SpringUtils Sonar Lint error.

--- java/03-bootstrap/src/main/java/com/fResult/bootstrap/common/SpringUtils.java
+++ java/03-bootstrap/src/main/java/com/fResult/bootstrap/common/SpringUtils.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.util.StringUtils;
 
 public abstract class SpringUtils {
+  private SpringUtils() {
+    throw new IllegalStateException("This cannot be instantiated");
+  }
+
   public static ConfigurableApplicationContext run(Class<?> componentClass, String profile) {
     final var applicationContext = new AnnotationConfigApplicationContext();
 
