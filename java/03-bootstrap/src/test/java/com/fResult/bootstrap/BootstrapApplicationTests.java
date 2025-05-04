0312dd87b926b3ae99922532a5b0ab79d6eeb191
fResult <styxmaz@gmail.com>
2025-05-05 01:55:11 +0700

    Remove `@SpringBootTest` which don't use yet.

--- java/03-bootstrap/src/test/java/com/fResult/bootstrap/BootstrapApplicationTests.java
+++ java/03-bootstrap/src/test/java/com/fResult/bootstrap/BootstrapApplicationTests.java
@@ -1,9 +1,7 @@
 package com.fResult.bootstrap;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BootstrapApplicationTests {
 
   @Test
