f573a6f3ca0ceb72c9f32540842041ba9106e2dd
fResult <styxmaz@gmail.com>
2025-05-04 17:24:00 +0700

    Implement of using `TransactionTemplateCustomerService` with application context in `BootstrapApplication` class for Kotlin.

--- kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
+++ kotlin/03-bootstrap/src/main/kotlin/com/fResult/bootstrap/BootstrapApplication.kt
@@ -1,24 +1,37 @@
 package com.fResult.bootstrap
 
-import com.fResult.bootstrap.common.DataSourceUtils
+import com.fResult.bootstrap.common.SpringUtils
+import com.fResult.bootstrap.common.configs.DataSourceConfiguration
+import com.fResult.bootstrap.customers.services.CustomerService
 import com.fResult.bootstrap.customers.services.TransactionTemplateCustomerService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
+import javax.sql.DataSource
 
-class BootstrapApplication
+@Configuration
+@Import(DataSourceConfiguration::class)
+class BootstrapApplication {
+  @Bean
+  fun transactionManager(dataSource: DataSource): PlatformTransactionManager = DataSourceTransactionManager(dataSource)
+
+  @Bean
+  fun customerService(
+    dataSource: DataSource,
+    transactionTemplate: TransactionTemplate,
+  ): TransactionTemplateCustomerService = TransactionTemplateCustomerService(dataSource, transactionTemplate)
+
+  @Bean
+  fun transactionTemplate(transactionManager: PlatformTransactionManager) = TransactionTemplate(transactionManager)
+}
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-  EmbeddedDatabaseBuilder()
-    .setType(EmbeddedDatabaseType.H2)
-    .build()
-    .let {
-      val initializedDataSource = DataSourceUtils.initializeDdl(it)
-      val dsTxManager = DataSourceTransactionManager(initializedDataSource)
-      val transactionTemplate = TransactionTemplate(dsTxManager)
+  val context = SpringUtils.run(BootstrapApplication::class, "prod")
+  val customerService = context.getBean(CustomerService::class.java)
 
-      TransactionTemplateCustomerService(initializedDataSource, transactionTemplate)
-    }.also { Demo.workWithCustomerService(BootstrapApplication::class, it) }
+  Demo.workWithCustomerService(BootstrapApplication::class, customerService)
 }
