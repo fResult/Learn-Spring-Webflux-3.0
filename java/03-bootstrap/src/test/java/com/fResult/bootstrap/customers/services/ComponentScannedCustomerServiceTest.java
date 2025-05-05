e3cb28c7b53d05f01405b89f472abdd6a2d2c7eb
fResult <styxmaz@gmail.com>
2025-05-05 13:57:07 +0700

    Create `ComponentScannedCustomerServiceTest` class for Java.

---
+++ java/03-bootstrap/src/test/java/com/fResult/bootstrap/customers/services/ComponentScannedCustomerServiceTest.java
@@ -0,0 +1,10 @@
+package com.fResult.bootstrap.customers.services;
+
+import com.fResult.bootstrap.BootstrapApplication;
+
+class ComponentScannedCustomerServiceTest extends ApplicationContextAwareBaseClass {
+  @Override
+  protected Class<BootstrapApplication> getConfigurationClass() {
+    return BootstrapApplication.class;
+  }
+}
