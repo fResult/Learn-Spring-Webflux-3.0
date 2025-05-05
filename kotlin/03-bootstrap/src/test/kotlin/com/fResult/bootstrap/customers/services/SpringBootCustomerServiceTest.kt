c87c5fb63d80dd3953d6acbf856bc39e6531c0f8
fResult <styxmaz@gmail.com>
2025-05-06 04:25:26 +0700

    Create `SpringBootCustomerServiceTest` class for Kotlin.

---
+++ kotlin/03-bootstrap/src/test/kotlin/com/fResult/bootstrap/customers/services/SpringBootCustomerServiceTest.kt
@@ -0,0 +1,20 @@
+package com.fResult.bootstrap.customers.services
+
+import com.fResult.bootstrap.TransactionTestMixin
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootCustomerServiceTest : BaseClass(), TransactionTestMixin {
+  @Autowired
+  private lateinit var customerService: CustomerService
+
+  override fun getCustomerService(): CustomerService = customerService
+
+  @Test
+  override fun `should insert customer`() {
+    super.`should insert customer`()
+    this.testTransactionalityOfSave(getCustomerService())
+  }
+}
