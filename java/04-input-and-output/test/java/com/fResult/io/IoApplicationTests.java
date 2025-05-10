f1a4f7f50908e153173c38a715e19945866cbd20
fResult <styxmaz@gmail.com>
2025-05-10 22:57:26 +0700

    Update test class name for Java module.

--- java/04-input-and-output/test/java/com/fResult/io/IoApplicationTests.java
+++ java/04-input-and-output/test/java/com/fResult/io/IoApplicationTests.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InputAndOutputApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class IoApplicationTests {
+  @Test
+  void contextLoads() {}
 }
