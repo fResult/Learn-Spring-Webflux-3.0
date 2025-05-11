97a27988083a95b00c2dea2a92dd71542f08e0b1
fResult <styxmaz@gmail.com>
2025-05-11 22:39:40 +0700

    Add module 04 with `Synchronous` class.

--- java/04-input-and-output/test/java/com/fResult/io/IoApplicationTests.java
+++ java/04-input-and-output/test/java/com/fResult/io/IoApplicationTests.java
@@ -1,10 +1,14 @@
 package com.fResult.io;
 
+import org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IoApplicationTests {
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    // Not implemented yet
+    assertTrue(true)
+  }
 }
