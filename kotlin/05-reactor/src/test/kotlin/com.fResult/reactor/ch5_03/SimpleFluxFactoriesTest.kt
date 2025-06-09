e44c0ecf347834c9e5ecb7d06d954e1177a7d972
fResult <styxmaz@gmail.com>
2025-06-10 00:54:14 +0700

    Try testing Flux of Char on module 05_03.

--- kotlin/05-reactor/src/test/kotlin/com.fResult/reactor/ch5_03/SimpleFluxFactoriesTest.kt
+++ kotlin/05-reactor/src/test/kotlin/com.fResult/reactor/ch5_03/SimpleFluxFactoriesTest.kt
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.test.StepVerifier
+
 class SimpleFluxFactoriesTest {
   @Test
   fun simple(): Unit {
     val rangeOfIntegers: Publisher<Int> = Flux.range(1, 10)
     StepVerifier.create(rangeOfIntegers).expectNextCount(10).verifyComplete()
+
+    val letters = Flux.just('A', 'B', 'C')
+    StepVerifier.create(letters).expectNext('A', 'B', 'C').verifyComplete()
   }
 }
