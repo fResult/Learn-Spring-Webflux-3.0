59074be289587342a4a119f09ecf035e0ef88ac9
fResult <styxmaz@gmail.com>
2025-06-10 00:54:36 +0700

    Try testing Mono of Date on module 05_03.

--- kotlin/05-reactor/src/test/kotlin/com.fResult/reactor/ch5_03/SimpleFluxFactoriesTest.kt
+++ kotlin/05-reactor/src/test/kotlin/com.fResult/reactor/ch5_03/SimpleFluxFactoriesTest.kt
@@ -3,7 +3,9 @@ package com.fResult.reactor.ch5_03
 import org.junit.jupiter.api.Test
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.util.*
 
 class SimpleFluxFactoriesTest {
   @Test
@@ -13,5 +15,9 @@ class SimpleFluxFactoriesTest {
 
     val letters = Flux.just('A', 'B', 'C')
     StepVerifier.create(letters).expectNext('A', 'B', 'C').verifyComplete()
+
+    val now = System.currentTimeMillis()
+    val greetingMono = Mono.just(Date(now))
+    StepVerifier.create(greetingMono).expectNext(Date(now)).verifyComplete()
   }
 }
