58f3c84011884e88fb89fd2f79a270eb468059e9
fResult <styxmaz@gmail.com>
2025-06-11 01:12:49 +0700

    Try testing Flux#take of String on module 05_06.

---
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_06/TakeTest.kt
@@ -0,0 +1,17 @@
+package com.fResult.reactor.ch5_06
+
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import kotlin.test.Test
+
+class TakeTest {
+  @Test
+  fun take() {
+    val count = 10L
+    val takenNumbers = range().take(count)
+    StepVerifier.create(takenNumbers).expectNextCount(count).verifyComplete()
+  }
+  }
+
+  private fun range() = Flux.range(0, 1000)
+}
