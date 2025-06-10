ed439c655fc02449141f8abac022b06d21c31e59
fResult <styxmaz@gmail.com>
2025-06-11 00:03:39 +0700

    Try testing SinkEmitter (EmitterProcessor) of Int from `Sinks.Many<String>` on module 05_04.

---
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_04/EmitterProcessorTest.kt
@@ -0,0 +1,30 @@
+package com.fResult.reactor.ch5_04
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Sinks
+import reactor.test.StepVerifier
+
+class EmitterProcessorTest {
+  @Test
+  fun emitterProcesser() {
+    val sinkEmitter = Sinks.many().multicast().onBackpressureBuffer<String>()
+    produce(sinkEmitter)
+    consume(sinkEmitter.asFlux())
+  }
+
+  private fun produce(sink: Sinks.Many<String>) {
+    sink.tryEmitNext("1")
+    sink.tryEmitNext("2")
+    sink.tryEmitNext("3")
+    sink.tryEmitComplete()
+  }
+
+  private fun consume(publisher: Flux<String>) {
+    StepVerifier.create(publisher)
+      .expectNext("1")
+      .expectNext("2")
+      .expectNext("3")
+      .verifyComplete()
+  }
+}
