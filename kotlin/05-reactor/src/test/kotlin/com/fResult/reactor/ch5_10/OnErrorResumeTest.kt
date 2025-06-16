6d5d636d3123cd2fd61c0cc28546d3e9182d51a4
fResult <styxmaz@gmail.com>
2025-06-17 02:43:30 +0700

    Try testing Reactor's error handlers on `Flux#onErrorResume` on module 05_10.

--- kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/OnErrorResumeTest.kt
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/OnErrorResumeTest.kt
@@ -9,6 +9,7 @@ class OnErrorResumeTest {
   private val log = LogManager.getLogger(OnErrorResumeTest::class.java)
 
   private val resultsInError1 = Flux.just(1, 2, 3).map(::failOn2)
+  private val resultsInError2 = Flux.just(1L, 2L, 3L).flatMap(::failOn2)
 
   @Test
   fun onErrorResume1() {
@@ -20,15 +21,37 @@ class OnErrorResumeTest {
       .verifyComplete()
   }
 
+  @Test
+  fun onErrorResume2() {
+    val longs = resultsInError2.onErrorResume(RuntimeException::class.java, ::fallbackOnError2)
+      .doOnComplete { log.info("Resume2 Completed") }
+
+    StepVerifier.create(longs)
+      .expectNext(1L, 3L, 2L, 1L) // Expect the fallback values after the error
+      .verifyComplete()
+  }
+
   private fun failOn2(counter: Int): Int = if (counter == 2)
     throw RuntimeException("Error on value $counter")
   else counter
+
+  private fun failOn2(counter: Long): Flux<Long> = if (counter == 2L)
+    Flux.error(RuntimeException("Error on value $counter"))
+  else
+    Flux.just(counter)
 
   private fun fallbackOnError1(ex: RuntimeException): Flux<Int> {
     log.error("Fallback1 for error: {}", ex.message)
 
     return Flux.just(3, 2, 1)
       .doOnComplete { log.info("Fallback1 completed") }
+  }
+
+  private fun fallbackOnError2(ex: RuntimeException): Flux<Long> {
+    log.error("Fallback2 for error: {}", ex.message)
+
+    return Flux.just(3L, 2L, 1L)
+      .doOnComplete { log.info("Fallback2 completed") }
   }
 }
 
