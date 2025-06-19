4a8a96069735e7f6a112f11510b2446f1ba0361b
fResult <styxmaz@gmail.com>
2025-06-20 00:20:56 +0700

    Try testing simple flux with `Flux#just` for String.

--- java/05-reactor/src/test/java/com/fResult/reactor/ch5_03/SimpleFluxFactoriesTest.java
+++ java/05-reactor/src/test/java/com/fResult/reactor/ch5_03/SimpleFluxFactoriesTest.java
@@ -2,10 +2,14 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
 public class SimpleFluxFactoriesTest {
   @Test
   void simple() {
     Publisher<Integer> rangeOfIntegers = Flux.range(0, 10);
     StepVerifier.create(rangeOfIntegers).expectNextCount(10).verifyComplete();
+
+    final var letters = Flux.just("A", "B", "C");
+    StepVerifier.create(letters).expectNext("A", "B", "C").verifyComplete();
   }
 }
