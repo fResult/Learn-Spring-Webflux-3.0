6a7d3e7bb5aeaa3793482647da964b37a46ea88c
fResult <styxmaz@gmail.com>
2025-06-20 01:22:00 +0700

    Move dependencies install to the test level

--- java/05-reactor/build.gradle.kts
+++ java/05-reactor/build.gradle.kts
@@ -23,18 +23,16 @@ repositories {
   mavenCentral()
 }
 
-val log4jVersion = "2.24.3"
 val blockHoundVersion = "1.0.13.RELEASE"
 
 dependencies {
   implementation(libs.spring.boot.starter.webflux)
-  compileOnly("org.projectlombok:lombok")
-  implementation("org.apache.logging.log4j:log4j-bom:$log4jVersion")
   developmentOnly(libs.spring.boot.devtools)
-  annotationProcessor("org.projectlombok:lombok")
   testImplementation(libs.spring.boot.starter.test)
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("io.projectreactor.tools:blockhound:$blockHoundVersion")
+  testCompileOnly("org.projectlombok:lombok")
+  testAnnotationProcessor("org.projectlombok:lombok")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
