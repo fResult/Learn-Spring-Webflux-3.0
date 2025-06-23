41ad4105ee6c6656d5da7284594b88213705a451
fResult <styxmaz@gmail.com>
2025-06-24 00:55:24 +0700

    Try testing Reactor's control flow on `Flux#merge` on module 05_10.

---
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowMergeTest.kt
@@ -0,0 +1,25 @@
+package com.fResult.reactor.ch5_10
+
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import kotlin.test.Test
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.toJavaDuration
+
+class ControlFlowMergeTest {
+  @Test
+  fun merge() {
+    val fastest = Flux.just(5, 6)
+    val secondFastest = Flux.just(1, 2).delayElements(2.milliseconds.toJavaDuration())
+    val slowest = Flux.just(3, 4).delayElements(20.milliseconds.toJavaDuration())
+
+    val streamOfStreams = Flux.just(slowest, secondFastest, fastest)
+    val merged = Flux.merge(streamOfStreams)
+    streamOfStreams.flatMap({ it }, 10)
+
+    StepVerifier.create(merged)
+      .expectNext(5, 6, 1, 2, 3, 4)
+      .expectComplete()
+      .verify()
+  }
+}
