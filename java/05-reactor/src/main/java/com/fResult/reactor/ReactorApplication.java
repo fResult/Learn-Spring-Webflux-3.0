a3cc13bdbcfb5cabfb43c7160c4771eec44bf534
fResult <styxmaz@gmail.com>
2025-06-28 00:06:56 +0700

    Create main class for Java module 05.

---
+++ java/05-reactor/src/main/java/com/fResult/reactor/ReactorApplication.java
@@ -0,0 +1,49 @@
+package com.fResult.reactor;
+
+import org.springframework.aop.SpringProxy;
+import org.springframework.aop.framework.Advised;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.DecoratingProxy;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+
+@SpringBootApplication
+@ImportRuntimeHints(ReactorApplication.Hints.class)
+public class ReactorApplication {
+  public static void main(String... args) {
+    SpringApplication.run(ReactorApplication.class, args);
+  }
+
+  static class Hints implements RuntimeHintsRegistrar {
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+      Flux.just(1);
+      final var prefix = "reactor.core.publisher.Traces";
+      final var classes =
+          List.of(
+              prefix,
+              prefix + "$StackWalkerCallSiteSupplierFactory",
+              prefix + "$SharedSecretsCallSiteSupplierFactory",
+              prefix + "$ExceptionCallSiteSupplierFactory");
+
+      classes.forEach(
+          c -> hints.reflection().registerType(TypeReference.of(c), MemberCategory.values()));
+
+      hints
+          .proxies()
+          .registerJdkProxy(
+              ScheduledExecutorService.class,
+              SpringProxy.class,
+              Advised.class,
+              DecoratingProxy.class);
+    }
+  }
+}
