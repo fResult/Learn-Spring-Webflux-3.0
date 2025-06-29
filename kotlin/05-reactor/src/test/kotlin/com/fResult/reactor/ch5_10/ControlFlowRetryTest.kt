10a2ccf2efec8a116ba5bd31563d47b6b7a606e3
fResult <styxmaz@gmail.com>
2025-06-29 15:35:00 +0700

    Refactor on `ControlFlowRetryTest#retry`.

--- kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowRetryTest.kt
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowRetryTest.kt
@@ -10,27 +10,31 @@ import kotlin.test.Test
 class ControlFlowRetryTest {
   private val log = LogManager.getLogger(ControlFlowRetryTest::class.java)
 
+  companion object {
+    private const val RETRY_COUNT = 2
+    private const val MESSAGE = "Hello, World!"
+  }
+
   @Test
   fun retry() {
     val producer = Flux.create(simulatedError2TimesThenEmitHello())
     val retryOnError = producer.retry()
 
-    StepVerifier.create(retryOnError).expectNext("Hello, World!").verifyComplete()
+    StepVerifier.create(retryOnError).expectNext(MESSAGE).verifyComplete()
   }
 
   private fun simulatedError2TimesThenEmitHello(): (FluxSink<String>) -> Unit {
     var tryingCounter = 0
 
     return { stringSink ->
-      val lessThan2 = ++tryingCounter <= 2
+      val lessThan2 = ++tryingCounter <= RETRY_COUNT
 
       if (lessThan2) {
         log.error("Emitting error: {} {} time(s)", RuntimeException::class.jvmName, tryingCounter)
         stringSink.error(RuntimeException("Simulated error: $tryingCounter time(s)"))
       } else {
-        val message = "Hello, World!"
-        log.info("Emitting {}", message)
-        stringSink.next(message)
+        log.info("Emitting {}", MESSAGE)
+        stringSink.next(MESSAGE)
         stringSink.complete()
       }
     }
