a1007c8e43aaf7fae58c0d63d2a34e19860edc5b
fResult <styxmaz@gmail.com>
2025-06-29 13:45:33 +0700

    Try testing Reactor's control flow on `Flux#retry` for Kotlin module 05_10.

---
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowRetryTest.kt
@@ -0,0 +1,35 @@
+package com.fResult.reactor.ch5_10
+
+import org.apache.logging.log4j.LogManager
+import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
+import reactor.test.StepVerifier
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.reflect.jvm.jvmName
+import kotlin.test.Test
+
+class ControlFlowRetryTest {
+  private val log = LogManager.getLogger(ControlFlowRetryTest::class.java)
+
+  @Test
+  fun retry() {
+    val errored = AtomicBoolean()
+    val producer = Flux.create(simulatedErrorThenEmitHello(errored))
+
+    val retryOnError = producer.retry()
+    StepVerifier.create(retryOnError).expectNext("Hello, World!").verifyComplete()
+  }
+
+  private fun simulatedErrorThenEmitHello(errored: AtomicBoolean): (FluxSink<String>) -> Unit = { stringSink ->
+    val isFirstTime = errored.compareAndSet(false, true)
+
+    if (isFirstTime) {
+      log.info("Emitting error: ${RuntimeException::class.jvmName}")
+      stringSink.error(RuntimeException("Simulated error"))
+    } else {
+      log.info("Emitting Hello, World!")
+      stringSink.next("Hello, World!")
+      stringSink.complete()
+    }
+  }
+}
