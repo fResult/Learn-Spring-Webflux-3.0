04c24070997ec422ef5f513fdfd471b70164f2a5
fResult <styxmaz@gmail.com>
2025-07-01 00:58:15 +0700

    Try testing Flux#flatMap to Pair for Java module 05_05.

---
+++ java/05-reactor/src/test/java/com/fResult/reactor/ch5_05/FlatMapTest.java
@@ -0,0 +1,24 @@
+package com.fResult.reactor.ch5_05;
+
+import java.time.Duration;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class FlatMapTest {
+  @Test
+  void flatMap() {
+    val data =
+        Flux.just(new Pair(1, 300), new Pair(2, 200), new Pair(3, 100))
+            .flatMap(id -> delayReplyFor(id.id(), id.delay()));
+
+    StepVerifier.create(data).expectNext(3, 2, 1).verifyComplete();
+  }
+
+  private Flux<Integer> delayReplyFor(int i, long delay) {
+    return Flux.just(i).delayElements(Duration.ofMillis(delay));
+  }
+
+  static record Pair(int id, long delay) {}
+}
