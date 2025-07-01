44dd3af67162e7f4f104feb58978c32d406ee66a
fResult <styxmaz@gmail.com>
2025-07-02 00:08:56 +0700

    Refactor by extracting method to be `zipWith` on ControlFlowZipTest#zip for Kotlin module 5

--- kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowZipTest.kt
+++ kotlin/05-reactor/src/test/kotlin/com/fResult/reactor/ch5_10/ControlFlowZipTest.kt
@@ -9,10 +9,12 @@ class ControlFlowZipTest {
   fun zip() {
     val first = Flux.just(1, 2, 3)
     val second = Flux.just("a", "b", "c")
-    val zipped = Flux.zip(first, second) { a, b -> "$a:$b" }
+    val zipped = Flux.zip(first, second, zipWith(":"))
 
     StepVerifier.create(zipped)
       .expectNext("1:a", "2:b", "3:c")
       .verifyComplete()
   }
+
+  private fun zipWith(delimiter: String): (a: Int, b: String) -> String = { a, b -> "$a$delimiter$b" }
 }
