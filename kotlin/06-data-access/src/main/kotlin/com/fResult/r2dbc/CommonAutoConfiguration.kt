8becba367f9b46d52daa15d3cbc9d22f47df92f0
fResult <styxmaz@gmail.com>
2025-07-12 20:05:48 +0700

    Create Kotlin module `06-data-access`.

---
+++ kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/CommonAutoConfiguration.kt
@@ -0,0 +1,39 @@
+package com.fResult.r2dbc
+
+import com.fResult.r2dbc.repositories.CustomerRepository
+import com.fResult.r2dbc.repositories.SimpleCustomerRepository
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.reactive.TransactionalOperator
+
+
+@Configuration
+class CommonAutoConfiguration {
+  companion object {
+    val log: Logger = LogManager.getLogger(CommonAutoConfiguration::class.java)
+  }
+
+  @Bean
+  fun defaultCustomerService(
+    customerRepository: CustomerRepository, // SimpleCustomerRepository,
+    transactionOperator: TransactionalOperator,
+    dbInitializer: CustomerDatabaseInitializer,
+  ): CustomerService {
+
+    return CustomerService(customerRepository, transactionOperator, dbInitializer)
+  }
+
+
+  @Bean
+  fun demo(repository: CustomerRepository): ApplicationListener<ApplicationReadyEvent> =
+    ApplicationListener { event ->
+      repository.save(Customer("fResult@exampl.com"))
+        .thenMany(repository.findAll())
+        .doOnEach(log::info)
+        .blockFirst()
+    }
+}
