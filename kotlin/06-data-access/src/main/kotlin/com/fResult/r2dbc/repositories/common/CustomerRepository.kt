f079a871d6c6eeb92582aa78f1cf168195842b6d
fResult <styxmaz@gmail.com>
2025-07-15 01:10:19 +0700

    Restructure directories and change type for Customer#id.

--- kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/repositories/common/CustomerRepository.kt
+++ kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/repositories/common/CustomerRepository.kt
@@ -1,9 +1,6 @@
-package com.fResult.r2dbc.repositories
+package com.fResult.r2dbc.repositories.common
 
 import com.fResult.r2dbc.Customer
-import com.fResult.r2dbc.repositories.SimpleCustomerRepository
-import org.springframework.data.r2dbc.repository.R2dbcRepository
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
@@ -13,40 +10,40 @@ import reactor.core.publisher.Mono
 class CustomerRepository(private val dbClient: DatabaseClient) : SimpleCustomerRepository {
   private fun map(row: Map<String, Any>) =
     Customer(
-      id = (row["id"] as Number).toInt(),
+      id = (row["id"] as Int).toString(),
       email = row["email"] as String
     )
 
   override fun save(customer: Customer): Mono<Customer> =
-    dbClient.sql("INSERT INTO customer ( email ) values ( $1 )")
-      .bind("$1", customer.email)
+    dbClient.sql("INSERT INTO customer ( email ) values ( :email )")
+      .bind("email", customer.email)
       .filter { stmt, _ -> stmt.returnGeneratedValues("id").execute() }
       .fetch()
       .first()
-      .flatMap { row -> findById(row["id"] as Int) }
+      .flatMap { (it["id"] as Int).toString().let(::findById) }
 
   override fun findAll(): Flux<Customer> =
     dbClient.sql("SELECT * FROM customer").fetch().all().`as` { it.map(::map) }
 
   override fun update(customer: Customer): Mono<Customer> =
-    dbClient.sql("UPDATE customer SET email = $1 WHERE id = $2")
-      .bind("$1", customer.email)
-      .bind("$2", customer.id!!)
+    dbClient.sql("UPDATE customer SET email = :email WHERE id = CAST(:id AS INTEGER)")
+      .bind("id", customer.id!!)
+      .bind("email", customer.email)
       .fetch()
       .first()
       .switchIfEmpty(Mono.empty())
       .then(findById(customer.id))
 
-  override fun findById(id: Int): Mono<Customer> =
-    dbClient.sql("SELECT * FROM customer WHERE id = $1")
-      .bind("$1", id)
+  override fun findById(id: String): Mono<Customer> =
+    dbClient.sql("SELECT * FROM customer WHERE id = CAST(:id AS INTEGER)")
+      .bind("id", id)
       .fetch()
       .first()
       .map(::map)
 
-  override fun deleteById(id: Int): Mono<Void> {
-    return dbClient.sql("DELETE FROM customer WHERE id = $1")
-      .bind("$1", id)
+  override fun deleteById(id: String): Mono<Void> {
+    return dbClient.sql("DELETE FROM customer WHERE id = CAST(:id AS INTEGER)")
+      .bind("id", id)
       .fetch()
       .rowsUpdated()
       .then()
