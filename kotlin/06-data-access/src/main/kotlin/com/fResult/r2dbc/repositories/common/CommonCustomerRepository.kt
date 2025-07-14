93128b332a4d705b616890eef3fe12789f4baa36
fResult <styxmaz@gmail.com>
2025-07-15 01:33:40 +0700

    Add logging for CommonCustomerRepository class.

--- kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/repositories/common/CommonCustomerRepository.kt
+++ kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/repositories/common/CommonCustomerRepository.kt
@@ -1,7 +1,10 @@
 package com.fResult.r2dbc.repositories.common
 
 import com.fResult.r2dbc.Customer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
@@ -9,12 +12,34 @@ import reactor.core.publisher.Mono
 
 @Repository
 @Profile("common")
-class CommonCustomerRepository(private val dbClient: DatabaseClient) : SimpleCustomerRepository {
-  private fun map(row: Map<String, Any>) =
+class CommonCustomerRepository(
+  private val dbClient: DatabaseClient,
+  private val environment: Environment,
+) : SimpleCustomerRepository {
+
+  companion object {
+    val log: Logger = LogManager.getLogger(CommonCustomerRepository::class.java)
+  }
+
+  private fun mapper(row: Map<String, Any>) =
     Customer(
       id = (row["id"] as Int).toString(),
       email = row["email"] as String
     )
+
+  init {
+    (environment.activeProfiles
+      .takeIf(Array<out String>::isNotEmpty)
+      ?.joinToString()
+      ?: "common")
+      .also { profile ->
+        log.info(
+          "[{}] initialized with profile: [{}]",
+          CommonCustomerRepository::class.simpleName,
+          profile,
+        )
+      }
+  }
 
   override fun save(customer: Customer): Mono<Customer> =
     dbClient.sql("INSERT INTO customer ( email ) values ( :email )")
@@ -25,7 +50,7 @@ class CommonCustomerRepository(private val dbClient: DatabaseClient) : SimpleCus
       .flatMap { (it["id"] as Int).toString().let(::findById) }
 
   override fun findAll(): Flux<Customer> =
-    dbClient.sql("SELECT * FROM customer").fetch().all().`as` { it.map(::map) }
+    dbClient.sql("SELECT * FROM customer").fetch().all().`as` { it.map(::mapper) }
 
   override fun update(customer: Customer): Mono<Customer> =
     dbClient.sql("UPDATE customer SET email = :email WHERE id = CAST(:id AS INTEGER)")
@@ -41,7 +66,7 @@ class CommonCustomerRepository(private val dbClient: DatabaseClient) : SimpleCus
       .bind("id", id)
       .fetch()
       .first()
-      .map(::map)
+      .map(::mapper)
 
   override fun deleteById(id: String): Mono<Void> {
     return dbClient.sql("DELETE FROM customer WHERE id = CAST(:id AS INTEGER)")
