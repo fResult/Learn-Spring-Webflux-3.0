88378169105421092e65606a115143a0baa67ef1
fResult <styxmaz@gmail.com>
2025-07-15 01:44:05 +0700

    Create spring beans for `CommonCustomerRepository` class and `springdata/CustomerRepository` interface.

---
+++ kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/configs/CustomerRepositoryConfiguration.kt
@@ -0,0 +1,28 @@
+package com.fResult.r2dbc.configs
+
+import com.fResult.r2dbc.repositories.common.CommonCustomerRepository
+import com.fResult.r2dbc.repositories.common.SimpleCustomerRepository
+import com.fResult.r2dbc.repositories.springdata.SpringDataCustomerRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import org.springframework.r2dbc.core.DatabaseClient
+import com.fResult.r2dbc.repositories.springdata.CustomerRepository as R2dbcCustomerRepository
+
+@Configuration
+class CustomerRepositoryConfiguration {
+  @Bean
+  @Profile("common")
+  fun commonCustomerRepository(
+    dbClient: DatabaseClient,
+    environment: Environment
+  ): SimpleCustomerRepository = CommonCustomerRepository(dbClient, environment)
+
+  @Bean
+  @Profile("springdata")
+  fun springDataCustomerRepository(
+    repository: R2dbcCustomerRepository,
+    environment: Environment
+  ): SimpleCustomerRepository = SpringDataCustomerRepository(repository, environment)
+}
