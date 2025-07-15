c148a5ce1ae8e69eb7b4aa026869e0b5e138ddb6
fResult <styxmaz@gmail.com>
2025-07-15 23:07:01 +0700

    Update root README and include Kotlin module `06-data-access` into root module.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "Learning Webflux 3.0"
 include("java:03-bootstrap")
 include("java:04-input-and-output")
 include("java:05-reactor")
+include("java:06-data-access")
 
 include("kotlin:03-bootstrap")
 // include("kotlin:04-input-and-output")
