649897fb1cbfa8dda5a4f1526487bb197986f14a
fResult <styxmaz@gmail.com>
2025-07-15 22:57:06 +0700

    Create `R2dbcCommonAutoConfiguration` class for testing db.

---
+++ kotlin/06-data-access/src/main/kotlin/com/fResult/r2dbc/configs/R2dbcCommonAutoConfiguration.kt
@@ -0,0 +1,49 @@
+package com.fResult.r2dbc.configs
+
+import com.fResult.r2dbc.Customer
+import com.fResult.r2dbc.CustomerDatabaseInitializer
+import com.fResult.r2dbc.CustomerService
+import com.fResult.r2dbc.repositories.common.SimpleCustomerRepository
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.transaction.reactive.TransactionalOperator
+
+@Configuration
+class R2dbcCommonAutoConfiguration {
+  companion object {
+    val log: Logger = LogManager.getLogger(R2dbcCommonAutoConfiguration::class.java)
+  }
+
+  @Bean
+  @Profile("springdata", "common")
+  fun commonCustomerService(
+    customerRepository: SimpleCustomerRepository,
+    transactionOperator: TransactionalOperator,
+    dbInitializer: CustomerDatabaseInitializer,
+  ): CustomerService {
+
+    log.info("Initializing commonCustomerService with common configuration")
+
+    return CustomerService(customerRepository, transactionOperator, dbInitializer)
+  }
+
+  @Bean
+  @Profile("springdata", "common")
+  fun demoCommon(repository: SimpleCustomerRepository): ApplicationListener<ApplicationReadyEvent> {
+
+    log.info("Registering demoCommon listener")
+
+    return ApplicationListener { event ->
+      repository.save(Customer("fResult@r2dbc.com"))
+        .thenMany(repository.findAll())
+        .doOnEach(log::info)
+        .blockFirst()
+    }
+  }
+}
