730e80c81313c48b746ed89df0d508fa4019de05
fResult <styxmaz@gmail.com>
2025-09-07 16:26:40 +0700

    Create `07-http`'s `webflux` sub-module.

---
+++ kotlin/07-http/webflux/src/main/kotlin/com/fResult/sse/SseRouteConfiguration.kt
@@ -0,0 +1,34 @@
+package com.fResult.sse
+
+import com.fResult.utils.IntervalMessageProducer
+import kotlinx.coroutines.reactive.asFlow
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class SseRouteConfiguration {
+  companion object {
+    private const val COUNT_PATH_VARIABLE: String = "count"
+    private val log: Logger = LogManager.getLogger(SseRouteConfiguration::class.java)
+  }
+
+  @Bean
+  fun routes(): RouterFunction<ServerResponse> = coRouter {
+    GET("/sse/{$COUNT_PATH_VARIABLE}", ::handleSse)
+  }
+
+  private suspend fun handleSse(request: ServerRequest): ServerResponse {
+    val count = request.pathVariable(COUNT_PATH_VARIABLE).toInt()
+    val publisher = IntervalMessageProducer.produce(count).doOnComplete {log.info("Completed SSE stream for count: $count")}
+
+    return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).bodyAndAwait(publisher.asFlow())
+  }
+}
