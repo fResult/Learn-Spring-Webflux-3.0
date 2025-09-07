0cb3bdca737f9dea7233098862404bea481a911a
fResult <styxmaz@gmail.com>
2025-09-07 16:26:40 +0700

    Create `07-http`'s `webflux` sub-module.

---
+++ kotlin/07-http/webflux/src/main/kotlin/com/fResult/http/customers/CustomerViewController.kt
@@ -0,0 +1,20 @@
+package com.fResult.http.customers
+
+import kotlinx.coroutines.reactive.asFlow
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class CustomerViewController(private val repository: CustomerRepository) {
+  @GetMapping("/c/customers.php")
+  fun customersView(model: Model): String {
+    val modelMap = mapOf(
+      "customers" to repository.findAll().asFlow(),
+      "type" to "@Controller",
+    )
+    model.addAllAttributes(modelMap)
+
+    return "customers"
+  }
+}
---
+++ kotlin/07-http/webflux/src/main/kotlin/com/fResult/http/routes/CustomRoutePredicatesConfiguration.kt
@@ -0,0 +1,42 @@
+package com.fResult.http.routes
+
+import com.fResult.http.routes.CaseInsensitiveRequestPredicates.Companion.i
+import com.fResult.utils.respondWithOkStreamBody
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.*
+import reactor.kotlin.core.publisher.toMono
+
+@Configuration
+class CustomRoutePredicatesConfiguration {
+  companion object {
+    private val HAD_PERMISSION_UID = setOf("1", "2", "3")
+  }
+
+  val handler: HandlerFunction<ServerResponse> = HandlerFunction { request ->
+    request.queryParam("name")
+      .toMono()
+      .map { "Hello, ${it.orElseGet { request.pathVariable("name") }}!" }
+      .let(::respondWithOkStreamBody)
+  }
+
+  @Bean
+  fun customRequestPredicates(): RouterFunction<ServerResponse> {
+    val peculiarRequestPredicate = RequestPredicates.GET("/test")
+      .and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
+      .and(::isRequestForValidUid)
+
+    val insensitiveRequestPredicate = i(RequestPredicates.GET("/greetings/{name}"))
+
+    return RouterFunctions.route()
+      .add(RouterFunctions.route(peculiarRequestPredicate, handler))
+      .add(RouterFunctions.route(insensitiveRequestPredicate, handler))
+      .build()
+  }
+
+  private fun isRequestForValidUid(request: ServerRequest): Boolean =
+    request.queryParam("uid")
+      .map(HAD_PERMISSION_UID::contains)
+      .orElse(false)
+}
