b0bd6140a267a9b209fb53e58e8f78ca0a23b90d
fResult <styxmaz@gmail.com>
2025-09-07 16:27:42 +0700

    Include build `07-http` under `kotlin` module.

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -4,6 +4,7 @@ includeBuild("03-bootstrap")
 includeBuild("04-input-and-output")
 includeBuild("05-reactor")
 includeBuild("06-data-access")
+includeBuild("07-http")
 
 dependencyResolutionManagement {
   versionCatalogs {
