27d0c4cb397db74a80bddb580a6d7a6e69ed8259
fResult <styxmaz@gmail.com>
2025-09-07 16:27:20 +0700

    Include `webflux` sub-module under `07-http`.

---
+++ kotlin/07-http/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "07-http"
+
+include("webflux")
+
+dependencyResolutionManagement {
+  versionCatalogs {
+    create("libs") {
+      from(files("../../gradle/libs.versions.toml"))
+    }
+  }
+}
