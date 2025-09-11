cbcd13d951f4e8279779a0dd2f77a21a1bace1ec
fResult <styxmaz@gmail.com>
2025-09-11 09:17:56 +0700

    Create `07-http`'s `security` sub-module.

---
+++ kotlin/07-http/security/src/main/kotlin/com/fresult/service/ServiceApplication.kt
@@ -0,0 +1,12 @@
+package com.fresult.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceApplication
+
+fun main(args: Array<String>) {
+  System.setProperty("spring.profiles.active", "service")
+  runApplication<ServiceApplication>(*args)
+}
