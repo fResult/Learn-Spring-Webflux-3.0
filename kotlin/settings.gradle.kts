f689cb28cde81011a635a6e2e48339887f4255f6
fResult <styxmaz@gmail.com>
2025-09-11 23:36:38 +0700

    Create `08-rsocket` for Kotlin module.

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -5,6 +5,7 @@ includeBuild("04-input-and-output")
 includeBuild("05-reactor")
 includeBuild("06-data-access")
 includeBuild("07-http")
+includeBuild("08-rsocket")
 
 dependencyResolutionManagement {
   versionCatalogs {
