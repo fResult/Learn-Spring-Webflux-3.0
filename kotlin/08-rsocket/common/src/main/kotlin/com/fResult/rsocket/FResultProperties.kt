b9e6745f3ebd261d7feae1fed35a0a09cbd7d183
fResult <styxmaz@gmail.com>
2025-09-13 00:13:01 +0700

    Create `08-rsocket`'s `common` sub-module for Kotlin.

---
+++ kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultProperties.kt
@@ -0,0 +1,12 @@
+package com.fResult.rsocket
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("fresult")
+class FResultProperties {
+  private val rsocket: RSocket = RSocket()
+
+  companion object {
+    class RSocket(var hostname: String = "localhost", var port: Int = 8181)
+  }
+}
--- kotlin/08-rsocket/settings.gradle.kts
+++ kotlin/08-rsocket/settings.gradle.kts
@@ -1,5 +1,7 @@
 rootProject.name = "08-rsocket"
 
+include("common")
+
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
