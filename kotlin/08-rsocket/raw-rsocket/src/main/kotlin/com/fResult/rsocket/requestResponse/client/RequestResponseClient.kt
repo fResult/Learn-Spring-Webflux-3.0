6586ce1835767845441c99b2b739efdde4d1b7d4
fResult <styxmaz@gmail.com>
2025-09-14 01:42:21 +0700

    Create RequestResponseClient with Raw RSocket Request/Response model.

---
+++ kotlin/08-rsocket/raw-rsocket/src/main/kotlin/com/fResult/rsocket/requestResponse/client/RequestResponseClient.kt
@@ -0,0 +1,47 @@
+package com.fResult.rsocket.requestResponse.client
+
+import com.fResult.rsocket.FResultProperties
+import io.rsocket.Payload
+import io.rsocket.core.RSocketClient
+import io.rsocket.core.RSocketConnector
+import io.rsocket.transport.netty.client.TcpClientTransport
+import io.rsocket.util.DefaultPayload
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import reactor.util.retry.Retry
+import kotlin.reflect.jvm.jvmName
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.toJavaDuration
+
+@Component
+class RequestResponseClient(private val props: FResultProperties) {
+  companion object {
+    private val log: Logger = LogManager.getLogger(RequestResponseClient::class.java)
+  }
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun onApplicationReady() {
+    log.info("Starting {} connection", RequestResponseClient::class.jvmName)
+
+    val clientTransport = TcpClientTransport.create(props.rsocket.hostname, props.rsocket.port)
+    val source = RSocketConnector.create()
+      .reconnect(Retry.backoff(50, 500.milliseconds.toJavaDuration()))
+      .connect(clientTransport)
+
+    val client = RSocketClient.from(source)
+
+    client.requestResponse(Mono.just(DefaultPayload.create("Reactive Spring")))
+      .doOnNext(::logAndReleasePayload)
+      .repeat(10)
+      .blockLast()
+  }
+
+  private fun logAndReleasePayload(payload: Payload) {
+    log.info("Received response data: {}", payload.dataUtf8)
+    payload.release()
+  }
+}
