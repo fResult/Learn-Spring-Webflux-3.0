14d5b4d976826df7130816e9b1175c1e7b817817
fResult <styxmaz@gmail.com>
2025-09-13 11:36:09 +0700

    Create `EncodingUtils` class and its spring bean.

--- kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultAutoConfiguration.kt
+++ kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultAutoConfiguration.kt
@@ -1,6 +1,11 @@
 package com.fResult.rsocket
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 
 @EnableConfigurationProperties(FResultProperties::class)
-class FResultAutoConfiguration
+class FResultAutoConfiguration {
+  @Bean
+  fun encodingUtils(objectMapper: ObjectMapper) = EncodingUtils(objectMapper)
+}
