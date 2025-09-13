efa3575b246bb08a9657d942ba26cededbc8ebe4
fResult <styxmaz@gmail.com>
2025-09-13 21:46:21 +0700

    Update the approach to do Auto Configuration following Spring Boot 3.0+.

--- kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultAutoConfiguration.kt
+++ kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultAutoConfiguration.kt
@@ -3,9 +3,14 @@ package com.fResult.rsocket
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 @EnableConfigurationProperties(FResultProperties::class)
 class FResultAutoConfiguration {
   @Bean
   fun encodingUtils(objectMapper: ObjectMapper) = EncodingUtils(objectMapper)
+
+  @Bean
+  fun objectMapper() = ObjectMapper()
 }
