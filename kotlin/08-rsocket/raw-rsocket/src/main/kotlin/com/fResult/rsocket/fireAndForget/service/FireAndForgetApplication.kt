847e104943383b6e3497d15da4ccc980ce8aa987
fResult <styxmaz@gmail.com>
2025-09-14 23:38:58 +0700

    Create `FireAndForgetApplication` for fireAndForget's service and client packages.

---
+++ kotlin/08-rsocket/raw-rsocket/src/main/kotlin/com/fResult/rsocket/fireAndForget/service/FireAndForgetApplication.kt
@@ -0,0 +1,13 @@
+package com.fResult.rsocket.fireAndForget.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FireAndForgetApplication
+
+@Throws(InterruptedException::class)
+fun main(args: Array<String>) {
+  runApplication<FireAndForgetApplication>(*args)
+  Thread.currentThread().join()
+}
