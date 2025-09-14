56739dbcbf8c2fbd9fb51700d02e2f6047e45a1a
fResult <styxmaz@gmail.com>
2025-09-14 22:05:13 +0700

    Register `bootRequestResponseService` and `bootRequestResponseClient` in `raw-rsocket`.

--- kotlin/08-rsocket/raw-rsocket/build.gradle.kts
+++ kotlin/08-rsocket/raw-rsocket/build.gradle.kts
@@ -1,3 +1,19 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 dependencies {
   implementation(project(":common"))
+}
+
+tasks.register<BootRun>("bootRequestResponseService") {
+  group = "application"
+  description = "Run the RSocket RequestResponse Server"
+  mainClass = "com.fResult.rsocket.requestResponse.service.RequestResponseApplicationKt"
+  classpath = sourceSets["main"].runtimeClasspath
+}
+
+tasks.register<BootRun>("bootRequestResponseClient") {
+  group = "application"
+  description = "Run the RSocket RequestResponse Client"
+  mainClass = "com.fResult.rsocket.requestResponse.client.RequestResponseApplicationKt"
+  classpath = sourceSets["main"].runtimeClasspath
 }
