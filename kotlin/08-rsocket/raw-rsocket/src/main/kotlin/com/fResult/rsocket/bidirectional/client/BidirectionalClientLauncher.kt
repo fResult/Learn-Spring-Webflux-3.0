edf9cd95ea09869871377cccc86c35d76c65d99d
fResult <styxmaz@gmail.com>
2025-09-19 07:35:29 +0700

    Create BidirectionalClientLauncher with Raw RSocket Bidirectional model.

---
+++ kotlin/08-rsocket/raw-rsocket/src/main/kotlin/com/fResult/rsocket/bidirectional/client/BidirectionalClientLauncher.kt
@@ -0,0 +1,37 @@
+package com.fResult.rsocket.bidirectional.client
+
+import com.fResult.rsocket.EncodingUtils
+import com.fResult.rsocket.FResultProperties
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import java.util.stream.IntStream
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+@Component
+class BidirectionalClientLauncher(
+  private val props: FResultProperties,
+  private val encodingUtils: EncodingUtils,
+) {
+  companion object {
+    val log: Logger = LogManager.getLogger(BidirectionalClientLauncher::class.java)
+  }
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun onApplicationReady() {
+    val maxClients = (5..10).random()
+    val hostname = props.rsocket.hostname
+    val port = props.rsocket.port
+    log.info("Launching {} clients connecting to {}:{}", maxClients, hostname, port)
+
+    Flux.fromStream(IntStream.range(0, maxClients).boxed())
+      .map { id -> BidirectionalClient(encodingUtils, id.toString(), hostname, port) }
+      .flatMap { client -> Flux.just(client).delayElements((1..30).random().seconds.toJavaDuration()) }
+      .flatMap(BidirectionalClient::getGreetings)
+      .subscribe(log::info)
+  }
+}
