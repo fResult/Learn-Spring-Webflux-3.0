e4f40060dd4da811065a1225ed93567b8e5f605b
fResult <styxmaz@gmail.com>
2025-09-22 22:48:08 +0700

    Move `Functions` object into proper package.

--- kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/EncodingUtils.kt
+++ kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/EncodingUtils.kt
@@ -1,6 +1,6 @@
 package com.fResult.rsocket
 
-import com.fResult.rsocket.dsl.fp.Functions.identity
+import com.fResult.rsocket.fp.Functions.identity
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
