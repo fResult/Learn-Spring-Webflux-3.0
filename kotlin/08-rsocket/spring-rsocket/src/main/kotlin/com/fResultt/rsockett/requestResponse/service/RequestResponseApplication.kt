31acf68f4cee629591b3909526f5a3cf67c83473
fResult <styxmaz@gmail.com>
2025-09-26 23:01:16 +0700

    Create RequestResponseApplication for Spring RSocket Request/Response model.

---
+++ kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResultt/rsockett/requestResponse/service/RequestResponseApplication.kt
@@ -0,0 +1,14 @@
+package com.fResult.rsocket.requestResponse.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestResponseApplication
+
+@Throws(InterruptedException::class)
+fun main(args: Array<String>) {
+  System.setProperty("spring.profiles.active", "service")
+
+  runApplication<RequestResponseApplication>(*args)
+}
