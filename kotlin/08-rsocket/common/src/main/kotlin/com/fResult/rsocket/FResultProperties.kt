13c3115f10bca2029662909be167485f013d9f9b
fResult <styxmaz@gmail.com>
2025-09-26 08:47:41 +0700

    Update `fresult.server.port`'s default value.

--- kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultProperties.kt
+++ kotlin/08-rsocket/common/src/main/kotlin/com/fResult/rsocket/FResultProperties.kt
@@ -7,6 +7,6 @@ class FResultProperties {
   val rsocket: RSocket = RSocket()
 
   companion object {
-    class RSocket(var hostname: String = "localhost", var port: Int = 8181)
+    class RSocket(var hostname: String = "localhost", var port: Int = 8182)
   }
 }
