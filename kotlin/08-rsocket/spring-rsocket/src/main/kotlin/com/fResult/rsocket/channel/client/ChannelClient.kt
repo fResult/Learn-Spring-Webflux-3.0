36a17bc66b4f9d67396c61d5d49842a7b8373190
fResult <styxmaz@gmail.com>
2025-09-28 01:16:19 +0700

    Create ChannelClient for Spring RSocket Channel (Stream) model.

---
+++ kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/channel/client/ChannelClient.kt
@@ -0,0 +1,56 @@
+package com.fResult.rsocket.channel.client
+
+import com.fResult.rsocket.dsl.retry.RetryConfigBuilder
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.messaging.rsocket.RSocketRequester
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.util.retry.Retry
+import reactor.util.retry.RetryBackoffSpec
+import java.nio.channels.ClosedChannelException
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+@Component
+class ChannelClient(private val requester: RSocketRequester) {
+  companion object {
+    val log: Logger = LogManager.getLogger(ChannelClient::class.java)
+  }
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun onApplicationReady() {
+    log.info("The data mime type is [{}]", requester.dataMimeType())
+    log.info("The metadata mime type is [{}]", requester.metadataMimeType())
+
+    Flux.interval(1.seconds.toJavaDuration())
+      .map { i -> "Ping #${i + 1}" }
+      .let(createPongRequest(requester))
+      .subscribe(::onPongSuccess, ::onPongFailure, ::onPongComplete)
+  }
+
+  private fun createPongRequest(requester: RSocketRequester): (Flux<String>) -> Flux<String> =
+    { pingFlux: Flux<String> ->
+      requester.route("pong")
+        .data(pingFlux.doOnNext(::logSendingRequest))
+        .retrieveFlux(String::class.java)
+        .retryWhen(retryBackoffOnClosedChannel { maxAttempts = 5 })
+    }
+
+  private fun logSendingRequest(ping: String): Unit = log.info("Sending request: {}", ping)
+  private fun onPongSuccess(pong: String): Unit = log.info("Received response: {}", pong)
+  private fun onPongFailure(ex: Throwable): Unit = log.error("Client stream failed with error: {}", ex.message, ex)
+  private fun onPongComplete(): Unit = log.info("Client stream completed")
+
+  private fun retryBackoffOnClosedChannel(init: RetryConfigBuilder.() -> Unit = {}): RetryBackoffSpec {
+    val cfg = RetryConfigBuilder().apply(init).build()
+
+    return Retry.backoff(cfg.maxAttempts, cfg.firstBackOff)
+      .maxBackoff(cfg.maxBackoff)
+      .filter { it is ClosedChannelException }
+      .jitter(0.2)
+      .onRetryExhaustedThrow { _, _ -> RuntimeException("Retries exhausted") }
+  }
+}
