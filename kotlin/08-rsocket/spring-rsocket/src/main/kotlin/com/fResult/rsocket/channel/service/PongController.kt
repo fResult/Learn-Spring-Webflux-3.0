ccd1d1d1f4694c2b8ef615447af05956f9345d51
fResult <styxmaz@gmail.com>
2025-09-28 01:19:19 +0700

    Refactor to domain-oriented FP style with explicit logging and message transformation
    - Extract logging to `logIncomingPing` and `logOutgoingPong`
    - Extract ping-to-pong transformation to `toPongMessage`
    - Improve code readability and maintainability in `PongController`

--- kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/channel/service/PongController.kt
+++ kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/channel/service/PongController.kt
@@ -1,5 +1,6 @@
 package com.fResult.rsocket.channel.service
 
+import com.fResult.rsocket.fp.then
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.messaging.handler.annotation.MessageMapping
@@ -14,6 +15,12 @@ class PongController {
   }
 
   @MessageMapping("pong")
-  fun pong(@Payload ping: Flux<String>): Flux<String> = ping.map { "pong" }
-    .doOnNext(log::info)
+  fun pong(@Payload ping: Flux<String>): Flux<String> =
+    ping.doOnNext(::logIncomingPing).map(::toPongMessage).doOnNext(::logOutgoingPong)
+
+  fun logIncomingPing(ping: String): Unit = log.info("Received request: {}", ping)
+  fun logOutgoingPong(pong: String): Unit = log.info("Sending response: {}", pong)
+  fun toPongMessage(request: String): String = request.let(::extractNumber then ::toPong)
+  fun extractNumber(ping: String): Int = ping.substringAfter("Ping #").trim().toInt()
+  fun toPong(number: Int): String = "Pong #$number"
 }
