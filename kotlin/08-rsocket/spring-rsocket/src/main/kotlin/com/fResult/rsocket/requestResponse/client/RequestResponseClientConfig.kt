488641048376162e8deac12bbe01cddb7edb615e
fResult <styxmaz@gmail.com>
2025-09-27 18:51:12 +0700

    Create RSocketRequester Bean for Spring RSocket Request/Response model.

---
+++ kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/requestResponse/client/RequestResponseClientConfig.kt
@@ -0,0 +1,28 @@
+package com.fResult.rsocket.requestResponse.client
+
+import com.fResult.rsocket.FResultProperties
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.messaging.rsocket.RSocketRequester
+
+@Configuration
+class RequestResponseClientConfig {
+  companion object {
+    private val log: Logger = LogManager.getLogger(RequestResponseClientConfig::class.java)
+  }
+
+  @Bean
+  fun rSocketRequester(
+    props: FResultProperties,
+    builder: RSocketRequester.Builder,
+  ): RSocketRequester {
+    log.info("RSocketRequester: {}:{}", props.rsocket.hostname, props.rsocket.port)
+
+    return builder
+      .dataMimeType(MediaType.APPLICATION_JSON)
+      .tcp(props.rsocket.hostname, props.rsocket.port)
+  }
+}
