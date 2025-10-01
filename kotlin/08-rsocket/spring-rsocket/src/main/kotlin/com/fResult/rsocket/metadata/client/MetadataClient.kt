a46b0129b9c4b9beacab69d21ef12b8b93777fdb
fResult <styxmaz@gmail.com>
2025-10-02 00:54:42 +0700

    Create `MetadataClient` class for Spring RSocket Metadata.

--- kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/metadata/client/MetadataClient.kt
+++ kotlin/08-rsocket/spring-rsocket/src/main/kotlin/com/fResult/rsocket/metadata/client/MetadataClient.kt
@@ -1,4 +1,26 @@
 package com.fResult.rsocket.metadata.client
 
-class MetadataClient {
+import com.fResult.rsocket.metadata.Constants
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.messaging.rsocket.RSocketRequester
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class MetadataClient(private val requester: RSocketRequester) {
+  @EventListener(ApplicationReadyEvent::class)
+  fun onApplicationReady() {
+    val chinese = requester.route("message")
+      .metadata(UUID.randomUUID().toString(), Constants.CLIENT_ID)
+      .metadata(Locale.CHINESE.language, Constants.LANG)
+      .send()
+
+    val english = requester.route("message")
+      .metadata(UUID.randomUUID().toString(), Constants.CLIENT_ID)
+      .metadata(Locale.ENGLISH.language, Constants.LANG)
+      .send()
+
+    chinese.then(english).subscribe()
+  }
 }
