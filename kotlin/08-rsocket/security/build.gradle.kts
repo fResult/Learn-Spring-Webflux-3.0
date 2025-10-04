c21d02b15366e89eb4f85a4224aa60438097e544
fResult <styxmaz@gmail.com>
2025-10-04 21:50:12 +0700

    Create `security` submodule under `08-rsocket` module.

---
+++ kotlin/08-rsocket/security/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+	implementation(libs.spring.boot.starter.security)
+	implementation(libs.spring.security.messaging)
+	implementation(libs.spring.security.rsocket)
+
+	testImplementation(libs.spring.security.test)
+}
---
+++ kotlin/08-rsocket/security/src/main/kotlin/com/fResult/security/service/SecurityApplication.kt
@@ -0,0 +1,12 @@
+package com.fResult.security.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityApplication
+
+fun main(args: Array<String>) {
+	System.setProperty("spring.profiles.active", "service")
+	runApplication<SecurityApplication>(*args)
+}
