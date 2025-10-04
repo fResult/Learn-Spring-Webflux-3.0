82ee9f7af1bafa95aa946f2810224dd4adcba116
fResult <styxmaz@gmail.com>
2025-10-04 22:34:16 +0700

    Register `bootServiceRun` and `bootClientRun` scripts in `08-rsocket:security`.

--- kotlin/08-rsocket/security/build.gradle.kts
+++ kotlin/08-rsocket/security/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 dependencies {
   implementation(project(":common"))
   implementation(libs.spring.boot.starter.security)
@@ -5,4 +7,23 @@ dependencies {
   implementation(libs.spring.security.rsocket)
 
   testImplementation(libs.spring.security.test)
+}
+
+springBoot {
+  mainClass.set("com.fResult.security.rsocket.SecurityApplicationKt")
+}
+
+tasks.register<BootRun>("bootServiceRun") {
+  group = "application"
+  description = "Run the application with the 'service' Spring profile"
+  mainClass.set("com.fResult.rsocket.service.SecurityApplicationKt")
+  classpath = sourceSets["main"].runtimeClasspath
+  systemProperty("spring.profiles.active", "service")
+}
+
+tasks.register<BootRun>("bootClientRun") {
+  group = "application"
+  description = "Run the application with the 'client' Spring profile"
+  mainClass.set("com.fResult.rsocket.client.SecurityApplicationKt")
+  classpath = sourceSets["main"].runtimeClasspath
 }
