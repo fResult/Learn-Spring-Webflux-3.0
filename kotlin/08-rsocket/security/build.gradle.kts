8e1676200ac7a73e3eff69d97e9bbe78b8e539f4
fResult <styxmaz@gmail.com>
2025-10-04 22:14:41 +0700

    Install `08-rsocket:common` module into `08-rsocket`' `ssecurity`.

--- kotlin/08-rsocket/security/build.gradle.kts
+++ kotlin/08-rsocket/security/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+	implementation(project(":common"))
 	implementation(libs.spring.boot.starter.security)
 	implementation(libs.spring.security.messaging)
 	implementation(libs.spring.security.rsocket)
