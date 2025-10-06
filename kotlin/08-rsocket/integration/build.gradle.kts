ed665d4d45f633d959283ae4086b57475e23b243
fResult <styxmaz@gmail.com>
2025-10-06 08:52:04 +0700

    Register `bootServiceRun` and `bootIntegrationRun` scripts in `08-rsocket:integration`.

--- kotlin/08-rsocket/integration/build.gradle.kts
+++ kotlin/08-rsocket/integration/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 dependencies {
   implementation(project(":common"))
   implementation("org.springframework.boot:spring-boot-starter-integration")
@@ -8,4 +10,19 @@ dependencies {
 
 springBoot {
   mainClass.set("com.fResult.rsocket.service.IntegrationApplicationKt")
+}
+
+tasks.register<BootRun>("bootServiceRun") {
+  group = "application"
+  description = "Run the application with the 'service' Spring profile"
+  mainClass.set("com.fResult.rsocket.service.IntegrationApplicationKt")
+  classpath = sourceSets["main"].runtimeClasspath
+  systemProperty("spring.profiles.active", "service")
+}
+
+tasks.register<BootRun>("bootIntegrationRun") {
+  group = "application"
+  description = "Run the application for the integration package"
+  mainClass.set("com.fResult.rsocket.integration.IntegrationApplicationKt")
+  classpath = sourceSets["main"].runtimeClasspath
 }
