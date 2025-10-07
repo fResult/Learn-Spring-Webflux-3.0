b8848d31219a8cbc6de5139c76e59f5a8cb8b76f
fResult <styxmaz@gmail.com>
2025-10-07 23:50:07 +0700

    Add log in `GreetingController` class for RSocket Integration.

--- kotlin/08-rsocket/integration/src/main/kotlin/com/fResult/rsocket/service/GreetingController.kt
+++ kotlin/08-rsocket/integration/src/main/kotlin/com/fResult/rsocket/service/GreetingController.kt
@@ -2,6 +2,8 @@ package com.fResult.rsocket.service
 
 import com.fResult.rsocket.dtos.GreetingRequest
 import com.fResult.rsocket.dtos.GreetingResponse
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
@@ -13,10 +15,17 @@ import kotlin.time.toJavaDuration
 
 @Controller
 class GreetingController {
+  companion object {
+    private val log: Logger = LogManager.getLogger(GreetingController::class.java)
+  }
+
   @OptIn(ExperimentalTime::class)
   @MessageMapping("greetings")
   fun greet(body: GreetingRequest): Flux<GreetingResponse> =
     Flux.fromStream { Stream.generate { GreetingResponse("Hello, ${body.name} @ ${Clock.System.now()}") } }
       .take(10)
       .delayElements(1.seconds.toJavaDuration())
+      .doOnNext(::onGreetSent)
+
+  fun onGreetSent(greet: GreetingResponse): Unit = log.info("onGreetSend: $greet")
 }
