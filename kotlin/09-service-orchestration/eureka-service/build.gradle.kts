07c1060873e5d1ef46e659cfc66bb7608c7cf94a
fResult <styxmaz@gmail.com>
2025-10-12 01:19:55 +0700

    Update `build.gradle.kts` for each service under `09-service-orchestration` module.

--- kotlin/09-service-orchestration/eureka-service/build.gradle.kts
+++ kotlin/09-service-orchestration/eureka-service/build.gradle.kts
@@ -1,11 +1,4 @@
-extra["springCloudVersion"] = "2025.0.0"
-
 dependencies {
-  implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
-}
-
-dependencyManagement {
-  imports {
-    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-  }
+  implementation(enforcedPlatform(libs.spring.cloud.dependencies.bom))
+  implementation(libs.spring.cloud.dependencies.bom)
 }
