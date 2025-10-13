d02d42863c158a078f3bd32a74598dd5187b5229
fResult <styxmaz@gmail.com>
2025-10-13 20:11:33 +0700

    Implement `error-service` submodule under `09-service-orchestration` module.

---
+++ kotlin/09-service-orchestration/error-service/src/main/kotlin/com/fResult/orchestration/ErrorRestController.kt
@@ -0,0 +1,59 @@
+package com.fResult.orchestration
+
+import org.springframework.boot.web.context.WebServerInitializedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.util.Loggers
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+
+@RestController
+class ErrorRestController {
+  companion object {
+    private val log = Loggers.getLogger(ErrorRestController::class.java)
+  }
+
+  private val port = AtomicInteger()
+  private val clientCounts = ConcurrentHashMap<String, AtomicInteger>()
+
+  @EventListener
+  fun onWebServerInitialized(event: WebServerInitializedEvent) {
+    log.info("Error service is running on port ${event.webServer.port}")
+
+    port.set(event.webServer.port)
+  }
+
+  @GetMapping("/ok")
+  fun okEndpoint(@RequestParam(required = false) clientId: String?): Mono<GreetingResponse> {
+    val countThusFar = registerAndCountClient(clientId)
+    return greetingResponse(countThusFar, port.get())
+  }
+
+  @GetMapping("/retry")
+  fun retryEndpoint(@RequestParam(required = false) clientId: String?): Mono<GreetingResponse> {
+    val countThusFar = registerAndCountClient(clientId)
+
+    return if (countThusFar > 2) greetingResponse(countThusFar, port.get())
+    else Mono.error(IllegalArgumentException())
+  }
+
+  @GetMapping("/circuit-breaker")
+  fun circuitBreakerEndpoint(@RequestParam(required = false) clientId: String?): Mono<Map<String, String>> {
+    registerAndCountClient(clientId)
+
+    return Mono.error(IllegalArgumentException())
+  }
+
+  private fun registerAndCountClient(clientId: String?): Int {
+    return clientId?.let {
+      clientCounts.computeIfAbsent(it) { AtomicInteger() }
+        .incrementAndGet()
+    } ?: 1
+  }
+
+  private fun greetingResponse(count: Int, port: Int): Mono<GreetingResponse> =
+    Mono.just(GreetingResponse("Greeting attempt # $count from port $port"))
+}
