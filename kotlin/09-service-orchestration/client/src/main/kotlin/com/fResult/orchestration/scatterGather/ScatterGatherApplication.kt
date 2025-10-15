c631f23cfeb94058b353b24a4cab689cc2e0be9b
fResult <styxmaz@gmail.com>
2025-10-15 23:47:57 +0700

    Create `ScatterGatherApplication`.

---
+++ kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/scatterGather/ScatterGatherApplication.kt
@@ -0,0 +1,11 @@
+package com.fResult.orchestration.scatterGather
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScatterGatherApplication
+
+fun main(args: Array<String>) {
+  runApplication<ScatterGatherApplication>(*args)
+}
