1f43839534dd523e0d17192904fdfb4b7ccf2b6e
fResult <styxmaz@gmail.com>
2025-10-16 01:10:09 +0700

    Create `OrderClient` to invoke to order service.

---
+++ kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/reactor/OrderClient.kt
@@ -0,0 +1,16 @@
+package com.fResult.orchestration.reactor
+
+import com.fResult.orchestration.Order
+import org.springframework.stereotype.Component
+import org.springframework.util.StringUtils
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+
+@Component
+class OrderClient(private val http: WebClient) {
+  fun getOrders(vararg ids: Int): Flux<Order> {
+    val ordersRoot = "http://order-service/orders?ids=${StringUtils.arrayToDelimitedString(ids.toTypedArray(), ",")}"
+
+    return http.get().uri(ordersRoot).retrieve().bodyToFlux(Order::class.java)
+  }
+}
