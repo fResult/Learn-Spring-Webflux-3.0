848dbf3b713c02f2e8c38668b176633b4271a524
fResult <styxmaz@gmail.com>
2025-10-16 01:12:26 +0700

    Create `DegradingClient` to continue running when error is occurred on Order Service.

---
+++ kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/reactor/DegradingClient.kt
@@ -0,0 +1,26 @@
+package com.fResult.orchestration.reactor
+
+import com.fResult.orchestration.Order
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.util.Loggers
+
+@Component
+class DegradingClient(private val orderClient: OrderClient) {
+  companion object {
+    private val log = Loggers.getLogger(DegradingClient::class.java)
+  }
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun onApplicationReady() {
+    orderClient.getOrders(1, 2)
+      .onErrorResume { ex -> Flux.empty() }
+      .subscribe(::onOrderReceived)
+  }
+
+  fun onOrderReceived(order: Order) {
+    log.info("Received order: $order")
+  }
+}
