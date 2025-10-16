04cb55b5132b512782741594c6e385b5150d89f1
fResult <styxmaz@gmail.com>
2025-10-17 02:33:52 +0700

    Install `resilience4j` dependencies for `client`'s resilience4j submodule.

--- kotlin/09-service-orchestration/client/build.gradle.kts
+++ kotlin/09-service-orchestration/client/build.gradle.kts
@@ -1,5 +1,17 @@
+val resilience4jVersion  = "2.3.0"
+
 dependencies {
   implementation(enforcedPlatform(libs.spring.cloud.dependencies.bom))
   implementation(libs.spring.cloud.starter.netflix.eureka.client)
   implementation(libs.spring.cloud.starter.loadbalancer)
+
+  implementation("io.github.resilience4j:resilience4j-ratelimiter:$resilience4jVersion")
+  implementation("io.github.resilience4j:resilience4j-circuitbreaker:$resilience4jVersion")
+  implementation("io.github.resilience4j:resilience4j-retry:$resilience4jVersion")
+  implementation("io.github.resilience4j:resilience4j-bulkhead:$resilience4jVersion")
+  implementation("io.github.resilience4j:resilience4j-reactor:$resilience4jVersion")
+}
+
+springBoot {
+  mainClass.set("com.fResult.orchestration.BasicApplicationKt")
 }
