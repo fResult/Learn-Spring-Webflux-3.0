def1e44a24256e040e528fd8de8ba52a2d90bf45
fResult <styxmaz@gmail.com>
2025-10-19 19:58:45 +0700

    Create `GlobalExceptionHandler` to catch IllegalArgumentException from controller.

---
+++ kotlin/09-service-orchestration/error-service/src/main/kotlin/com/fResult/orchestration/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.fResult.orchestration
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+  @ExceptionHandler(IllegalArgumentException::class)
+  fun handleIllegalArgument(ex: IllegalArgumentException): Mono<ResponseEntity<ProblemDetail>> =
+    ProblemDetail.forStatusAndDetail(
+      HttpStatus.BAD_REQUEST,
+      "${IllegalArgumentException::class.simpleName}: ${ex.message ?: "Invalid argument"}",
+    )
+      .toMono()
+      .map(ResponseEntity.badRequest()::body)
+}
