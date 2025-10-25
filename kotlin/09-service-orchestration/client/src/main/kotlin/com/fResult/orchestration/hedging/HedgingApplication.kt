7bf121572d96e51a2619aa06fc004073b7821e5c
fResult <styxmaz@gmail.com>
2025-10-25 22:10:50 +0700

    Enable `HedgingLoadBalancerProperties` class as props type safety.

--- kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/hedging/HedgingApplication.kt
+++ kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/hedging/HedgingApplication.kt
@@ -1,9 +1,11 @@
 package com.fResult.orchestration.hedging
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(HedgingLoadBalancerProperties::class)
 class HedgingApplication
 
 fun main(args: Array<String>) {
