05874816c57adddb3c1734613913d5653fc270ae
fResult <styxmaz@gmail.com>
2025-10-31 00:52:21 +0700

    Create GET `/profile?customer-id=<customer_id>` invocation in ScatterGather.

--- kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/scatterGather/CrmClient.kt
+++ kotlin/09-service-orchestration/client/src/main/kotlin/com/fResult/orchestration/scatterGather/CrmClient.kt
@@ -2,10 +2,12 @@ package com.fResult.orchestration.scatterGather
 
 import com.fResult.orchestration.Customer
 import com.fResult.orchestration.Order
+import com.fResult.orchestration.Profile
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Component
 class CrmClient(private val http: WebClient) {
@@ -19,5 +21,11 @@ class CrmClient(private val http: WebClient) {
     val ordersRoot = "http://order-service/orders?customer-ids=${StringUtils.arrayToDelimitedString(customerIds, ",")}"
 
     return http.get().uri(ordersRoot).retrieve().bodyToFlux(Order::class.java)
+  }
+
+  fun getProfile(customerId: Int): Mono<Profile> {
+    val profilesRoot = "http://profile-service/profiles?customer-id=$customerId"
+
+    return http.get().uri(profilesRoot).retrieve().bodyToMono(Profile::class.java)
   }
 }
