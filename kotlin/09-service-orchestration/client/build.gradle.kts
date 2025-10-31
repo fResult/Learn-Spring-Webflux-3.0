e0769684496da014e8875cd4110d5ffa35628623
fResult <styxmaz@gmail.com>
2025-10-31 23:01:25 +0700

    Replace hardcoded Resilience4j versions with type-safe catalog references.

--- kotlin/09-service-orchestration/client/build.gradle.kts
+++ kotlin/09-service-orchestration/client/build.gradle.kts
@@ -7,11 +7,11 @@ dependencies {
   implementation(libs.spring.cloud.starter.netflix.eureka.client)
   implementation(libs.spring.cloud.starter.loadbalancer)
 
-  implementation("io.github.resilience4j:resilience4j-ratelimiter:$resilience4jVersion")
-  implementation("io.github.resilience4j:resilience4j-circuitbreaker:$resilience4jVersion")
-  implementation("io.github.resilience4j:resilience4j-retry:$resilience4jVersion")
-  implementation("io.github.resilience4j:resilience4j-bulkhead:$resilience4jVersion")
-  implementation("io.github.resilience4j:resilience4j-reactor:$resilience4jVersion")
+  implementation(libs.resilience4j.ratelimiter)
+  implementation(libs.resilience4j.circuitbreaker)
+  implementation(libs.resilience4j.retry)
+  implementation(libs.resilience4j.bulkhead)
+  implementation(libs.resilience4j.reactor)
 }
 
 springBoot {
